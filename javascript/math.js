ae88ae4cd2593d44ca122e5330da9741528eff9a
Savita <96195601+savigurjar@users.noreply.github.com>
2024-07-23 21:01:42 +0530

    Add files via upload

---
+++ javascript/math.js
@@ -0,0 +1,20 @@
+// const hundred = 1000;
+// console.log(Math) //math is object
+
+// console.log(Math.abs(-4)); //minus value in positive
+
+// console.log(Math.round(7.13));  //0 and 1 ke bich value
+// console.log(Math.ceil(7.10));
+// console.log(Math.floor(7.10));
+// console.log(Math.min(4,3,6 ,8))
+// console.log(Math.max(4,3,6,8));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+// Math.random() * (max - min + 1) + min
+console.log(Math.floor(Math.random()*(max-min+1))+min)
